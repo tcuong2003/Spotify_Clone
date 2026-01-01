7145fb4a6b9ec291a540d2b61b4cc4648c9f04c0
mac <mac@Macbook-of-TC.local>
2026-01-01 12:34:36 +0700

    feat: build docker

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,17 +8,17 @@
             type="button"
             class="text-white text-2xl font-semibold hover:underline cursor-pointer"
         >
-            Podcasts to make you smarter
+            Made for you
         </button>
 
         <div class="py-1.5"></div>
 
         <div class="flex items-center">
-            <HomeCard image="https://picsum.photos/id/30/300/300" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard image="https://picsum.photos/id/45/300/300" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="md:block hidden" image="https://picsum.photos/id/65/300/300" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="lg:block hidden" image="https://picsum.photos/id/67/300/300" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="xl:block hidden" image="https://picsum.photos/id/100/300/300" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard image="https://picsum.photos/id/30/300/300" title="Discover Weekly" subTitle="Your weekly mixtape of fresh music. Enjoy new music and deep cuts picked for you." />
+            <HomeCard image="https://picsum.photos/id/45/300/300" title="Release Radar" subTitle="Catch all the latest music from artists you follow, plus new singles picked for you." />
+            <HomeCard class="md:block hidden" image="https://picsum.photos/id/65/300/300" title="Daily Mix 1" subTitle="Made for you" />
+            <HomeCard class="lg:block hidden" image="https://picsum.photos/id/67/300/300" title="Daily Mix 2" subTitle="Made for you" />
+            <HomeCard class="xl:block hidden" image="https://picsum.photos/id/100/300/300" title="Daily Mix 3" subTitle="Made for you" />
         </div>
     </div>
 
@@ -27,17 +27,17 @@
             type="button"
             class="text-white text-2xl font-semibold hover:underline cursor-pointer"
         >
-        Recommended radio
+        Recently played
         </button>
 
         <div class="py-1.5"></div>
 
         <div class="flex items-center">
-            <HomeCard image="https://picsum.photos/id/120/300/300" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard image="https://picsum.photos/id/110/300/300" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="md:block hidden" image="https://picsum.photos/id/221/300/300" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="lg:block hidden" image="https://picsum.photos/id/232/300/300" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="xl:block hidden" image="https://picsum.photos/id/187/300/300" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard image="https://picsum.photos/id/120/300/300" title="Liked Songs" subTitle="Playlist • 127 songs" />
+            <HomeCard image="https://picsum.photos/id/110/300/300" title="Today's Top Hits" subTitle="Playlist • Spotify" />
+            <HomeCard class="md:block hidden" image="https://picsum.photos/id/221/300/300" title="RapCaviar" subTitle="Playlist • Spotify" />
+            <HomeCard class="lg:block hidden" image="https://picsum.photos/id/232/300/300" title="Chill Hits" subTitle="Playlist • Spotify" />
+            <HomeCard class="xl:block hidden" image="https://picsum.photos/id/187/300/300" title="Rock Classics" subTitle="Playlist • Spotify" />
         </div>
     </div>
 
@@ -52,11 +52,11 @@
         <div class="py-1.5"></div>
 
         <div class="flex items-center">
-            <HomeCard image="https://picsum.photos/id/88/300/300" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard image="https://picsum.photos/id/243/300/300" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="md:block hidden" image="https://picsum.photos/id/123/300/300" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="lg:block hidden" image="https://picsum.photos/id/121/300/300" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="xl:block hidden" image="https://picsum.photos/id/99/300/300" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard image="https://picsum.photos/id/88/300/300" title="Pop Rising" subTitle="Playlist • Spotify" />
+            <HomeCard image="https://picsum.photos/id/243/300/300" title="Indie Pop" subTitle="Playlist • Spotify" />
+            <HomeCard class="md:block hidden" image="https://picsum.photos/id/123/300/300" title="Alternative Rock" subTitle="Playlist • Spotify" />
+            <HomeCard class="lg:block hidden" image="https://picsum.photos/id/121/300/300" title="Hip Hop Central" subTitle="Playlist • Spotify" />
+            <HomeCard class="xl:block hidden" image="https://picsum.photos/id/99/300/300" title="Jazz Vibes" subTitle="Playlist • Spotify" />
         </div>
     </div>
 </template>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -14,46 +14,46 @@
         <div class="py-1.5"></div>
 
         <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6">
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/40/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/45/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/76/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/56/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/25/300/300"/>
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/103/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/244/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/202/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/101/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/120/300/300"/>
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/40/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/45/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/76/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/56/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/25/300/300"/>
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/103/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/244/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/202/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/101/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/120/300/300"/>
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/40/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/45/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/76/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/56/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/25/300/300"/>
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/103/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/244/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/202/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/101/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/120/300/300"/>
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/40/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/45/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/76/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/56/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/25/300/300"/>
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/103/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/244/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/202/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/101/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/120/300/300"/>
+            <CategorySelect category="Pop" image="https://picsum.photos/id/40/300/300"/>
+            <CategorySelect category="Hip-Hop" image="https://picsum.photos/id/45/300/300"/>
+            <CategorySelect category="Rock" image="https://picsum.photos/id/76/300/300"/>
+            <CategorySelect category="Jazz" image="https://picsum.photos/id/56/300/300"/>
+            <CategorySelect category="Electronic" image="https://picsum.photos/id/25/300/300"/>
+            <CategorySelect category="Country" image="https://picsum.photos/id/103/300/300"/>
+            <CategorySelect category="R&B" image="https://picsum.photos/id/244/300/300"/>
+            <CategorySelect category="Classical" image="https://picsum.photos/id/202/300/300"/>
+            <CategorySelect category="Indie" image="https://picsum.photos/id/101/300/300"/>
+            <CategorySelect category="Reggae" image="https://picsum.photos/id/120/300/300"/>
+            <CategorySelect category="Blues" image="https://picsum.photos/id/40/300/300"/>
+            <CategorySelect category="Folk" image="https://picsum.photos/id/45/300/300"/>
+            <CategorySelect category="Metal" image="https://picsum.photos/id/76/300/300"/>
+            <CategorySelect category="Punk" image="https://picsum.photos/id/56/300/300"/>
+            <CategorySelect category="Soul" image="https://picsum.photos/id/25/300/300"/>
+            <CategorySelect category="Alternative" image="https://picsum.photos/id/103/300/300"/>
+            <CategorySelect category="Dance" image="https://picsum.photos/id/244/300/300"/>
+            <CategorySelect category="Ambient" image="https://picsum.photos/id/202/300/300"/>
+            <CategorySelect category="World" image="https://picsum.photos/id/101/300/300"/>
+            <CategorySelect category="Latin" image="https://picsum.photos/id/120/300/300"/>
+            <CategorySelect category="Pop" image="https://picsum.photos/id/40/300/300"/>
+            <CategorySelect category="Hip-Hop" image="https://picsum.photos/id/45/300/300"/>
+            <CategorySelect category="Rock" image="https://picsum.photos/id/76/300/300"/>
+            <CategorySelect category="Jazz" image="https://picsum.photos/id/56/300/300"/>
+            <CategorySelect category="Electronic" image="https://picsum.photos/id/25/300/300"/>
+            <CategorySelect category="Country" image="https://picsum.photos/id/103/300/300"/>
+            <CategorySelect category="R&B" image="https://picsum.photos/id/244/300/300"/>
+            <CategorySelect category="Classical" image="https://picsum.photos/id/202/300/300"/>
+            <CategorySelect category="Indie" image="https://picsum.photos/id/101/300/300"/>
+            <CategorySelect category="Reggae" image="https://picsum.photos/id/120/300/300"/>
+            <CategorySelect category="Blues" image="https://picsum.photos/id/40/300/300"/>
+            <CategorySelect category="Folk" image="https://picsum.photos/id/45/300/300"/>
+            <CategorySelect category="Metal" image="https://picsum.photos/id/76/300/300"/>
+            <CategorySelect category="Punk" image="https://picsum.photos/id/56/300/300"/>
+            <CategorySelect category="Soul" image="https://picsum.photos/id/25/300/300"/>
+            <CategorySelect category="Alternative" image="https://picsum.photos/id/103/300/300"/>
+            <CategorySelect category="Dance" image="https://picsum.photos/id/244/300/300"/>
+            <CategorySelect category="Ambient" image="https://picsum.photos/id/202/300/300"/>
+            <CategorySelect category="World" image="https://picsum.photos/id/101/300/300"/>
+            <CategorySelect category="Latin" image="https://picsum.photos/id/120/300/300"/>
         </div>
     </div>
 </template>
